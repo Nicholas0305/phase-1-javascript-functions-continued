f8e96003209a07ad74bd8a62f22a91d5c2345a1f
Nicholas <najernigin@gmail.com>
2023-10-31 14:20:31 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(message="roller-skate"){
+
+return (`This Saturday, I want to ${message}!`)
+}
+function mondayWork(message="go to the office"){
+    return(`This Monday, I will ${message}.`)
+}
+function wrapAdjective(highlight = "*") {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
